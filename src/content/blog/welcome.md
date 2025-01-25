47a26d58913b46d4f0375f0cb789029de64bc435
Dan Denney <dd.subscriptions@gmail.com>
2025-01-24 20:49:50 -0600

    feat(astro): add starter features

---
+++ src/content/blog/welcome.md
@@ -0,0 +1,7 @@
+---
+title: "Welcome to My Astro Photo Blog"
+date: 2024-03-20
+tags: ["welcome", "introduction"]
+---
+
+Welcome to my photo blog! Here I'll share amazing astronomical photographs and write about space-related topics. 
---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+import { defineCollection, z } from 'astro:content';
+
+const photoCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    image: z.string(),
+    attribution: z.string(),
+    tags: z.array(z.string()),
+    date: z.date(),
+  }),
+});
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  'photos': photoCollection,
+  'blog': blogCollection,
+}; 
